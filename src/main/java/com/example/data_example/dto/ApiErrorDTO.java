a40ad55287848adaa6b699b6b2078b272ee3fe63
Mr-Charlesworth <andrew.charlesworth@sheffield.ac.uk>
2025-10-31 10:39:20 +0000

    added APIErrorDTO

--- src/main/java/com/example/data_example/dto/ApiErrorDTO.java
+++ src/main/java/com/example/data_example/dto/ApiErrorDTO.java
@@ -4,4 +4,25 @@ public class ApiErrorDTO {
 
     private int status;
     private String error;
+
+    public ApiErrorDTO(int status, String error) {
+        this.status = status;
+        this.error = error;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
 }
